3cd1e141f84071c45411cec6695b66c729e41fcc
Ariadna Ponce <ariadna74.m@gmail.com>
2024-06-24 14:12:35 -0500

    Update Circulo.py

--- src/classes/Circulo.py
+++ src/classes/Circulo.py
@@ -1,12 +1,15 @@
 from .Figura import Figura
 
-import pygame
+import matplotlib.pyplot as plt
 
 class Circulo(Figura):
     def __init__(self, params):
         super().__init__('CIRCULO', params)
 
-    def draw(self, screen, color, font):
-        pygame.draw.circle(screen, color, self.params['vertex'], int(self.params['radio']), 2)
-        pygame.draw.circle(screen, color, self.params['vertex'], 5)
-        self.show_coordinates(screen, font, self.params['vertex'])
+    def draw(self, ax):
+        vertex = self.params['vertex'][0]
+        x, y = vertex[0], vertex[1]
+        radio = self.params['radio']
+        circle = plt.Circle((x, y), radio, linewidth=2, edgecolor='black', facecolor='none')
+        ax.add_artist(circle)
+
