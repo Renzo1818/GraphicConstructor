f36513725665ee339984fdb2bcfdcf7c76419d5d
Ariadna Ponce <ariadna74.m@gmail.com>
2024-06-24 01:06:52 -0500

    Update Figura.py
    Actualización de la clase Figura

--- src/classes/Figura.py
+++ src/classes/Figura.py
@@ -1,13 +1,9 @@
-import pygame
+import matplotlib.pyplot as plt
 
 class Figura:
     def __init__(self, tipo, params):
         self.tipo = tipo
         self.params = params
 
-    def draw(self, screen, color, font):
+    def draw(self):
         pass
-
-    def show_coordinates(self, screen, font, vertex):
-        label = font.render(f"({vertex[0]}, {vertex[1]})", True, (0, 0, 0))
-        screen.blit(label, (vertex[0] + 5, vertex[1] - 15))
