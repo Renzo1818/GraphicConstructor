8eed2fb2cc7d30db1dc6bed0252068710e848ff6
Ariadna Ponce <ariadna74.m@gmail.com>
2024-06-24 14:15:04 -0500

    Update Triangulo.py
    Actualización de la clase Triángulo, Rectángulo y Circulo

--- src/classes/Triangulo.py
+++ src/classes/Triangulo.py
@@ -1,17 +1,12 @@
 from .Figura import Figura
 
-import pygame
+import matplotlib.pyplot as plt
 
 class Triangulo(Figura):
     def __init__(self, params):
         super().__init__('TRIANGULO', params)
 
-    def draw(self, screen, color, font):
-        x, y = self.params['vertex']
-        width = self.params['ancho']
-        height = self.params['alto']
-        points = [(x, y), (x + width / 2, y - height), (x + width, y)]
-        pygame.draw.polygon(screen, color, points, 2)
-        for vx, vy in points:
-            pygame.draw.circle(screen, color, (vx, vy), 5)
-            self.show_coordinates(screen, font, (vx, vy))
+    def draw(self, ax):
+        vertices = [list(vertex) for vertex in self.params['vertex']]  # Convertir vértices a lista de listas
+        triangle = plt.Polygon(vertices, linewidth=2, edgecolor='black', facecolor='none')
+        ax.add_artist(triangle)
