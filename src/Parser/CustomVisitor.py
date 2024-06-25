c809b720b2b7bc6927b8a00aa27a19822dee8f66
RenzoZ <93232895+Renzo1818@users.noreply.github.com>
2024-06-24 22:29:35 -0500

    Se agrego funcionalidad al archivo CustomVisitor.py

--- src/Parser/CustomVisitor.py
+++ src/Parser/CustomVisitor.py
@@ -12,7 +12,32 @@ from src.Parser.GraphLangVisitor import GraphLangVisitor
 
 class CustomVisitor(GraphLangVisitor):
     def __init__(self):
-        pass
+        self.variables = {}
+        self.shapes = {}
+        self.scene = {}
+        self.scene_invocada = False
+        self.width = 800
+        self.height = 600
+        self.transformations = {}
 
-    def visitImprimir(self, ctx:GraphLangParser.ImprimirContext):
-        return self.visitChildren(ctx)
+    def visitSentenciaFor(self, ctx:GraphLangParser.SentenciaForContext):
+        # Evaluación de la inicialización
+        self.visit(ctx.declaracionVariable())
+        
+        # Evaluación de la condición de continuación
+        while self.visit(ctx.expresionComparativa()):
+            # Ejecución del bloque
+            self.visit(ctx.bloque())
+            
+            # Evaluación de la expresión de actualización
+            self.visit(ctx.declaracionIncremental())
+
+        return None
+    
+    # Regla sentenciaWhile
+    def visitSentenciaWhile(self, ctx: GraphLangParser.SentenciaWhileContext):
+        condicion = self.visit(ctx.expresion())  # Visitamos la expresión de la condición
+        while condicion:
+            self.visit(ctx.bloque())  # Visitamos el bloque del while
+            condicion = self.visit(ctx.expresion())  # Evaluamos la condición nuevamente al final del bloque
+        return None
