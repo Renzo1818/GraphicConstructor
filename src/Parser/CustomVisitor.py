8acfa08092310c26282e71b307292d0c2480d696
RenzoZ <93232895+Renzo1818@users.noreply.github.com>
2024-06-27 11:46:14 -0500

    Se agrego funcionalidad a CustomVisitor y main

--- src/Parser/CustomVisitor.py
+++ src/Parser/CustomVisitor.py
@@ -1,7 +1,3 @@
-import pygame
-import sys
-
-
 from ..classes.Circulo import Circulo
 from ..classes.Rectangulo import Rectangulo
 from ..classes.Cuadrado import Cuadrado
@@ -9,6 +5,9 @@ from ..classes.Triangulo import Triangulo
 
 from src.Parser.GraphLangParser import GraphLangParser
 from src.Parser.GraphLangVisitor import GraphLangVisitor
+
+import matplotlib.pyplot as plt
+import copy
 
 class CustomVisitor(GraphLangVisitor):
     def __init__(self):
@@ -41,3 +40,51 @@ class CustomVisitor(GraphLangVisitor):
             self.visit(ctx.bloque())  # Visitamos el bloque del while
             condicion = self.visit(ctx.expresion())  # Evaluamos la condición nuevamente al final del bloque
         return None
+    
+    # Regla declaracionIncremental
+    def visitDeclaracionIncremental(self, ctx: GraphLangParser.DeclaracionIncrementalContext):
+        variable_name = ctx.ID().getText()
+        increment_operator = ctx.opIncrementales().getText()
+        if increment_operator == '++':
+            self.variables[variable_name] += 1
+        elif increment_operator == '--':
+            self.variables[variable_name] -= 1
+        return None
+    
+    # Regla opIncrementales
+    def visitOpIncrementales(self, ctx: GraphLangParser.OpIncrementalesContext):
+        return ctx.getText()
+    
+    # Bloque
+    def visitBloque(self, ctx: GraphLangParser.BloqueContext):
+        for sentencia in ctx.sentencia():
+            self.visit(sentencia)
+        return None
+    
+    def render_scene(self):
+        if not self.scene_invocada:
+            print("Error: No se ha declarado una escena para renderizar.")
+            return
+
+        # Definir los límites del plano cartesiano
+        x_min, x_max = -50, 50
+        y_min, y_max = -50, 50
+
+        # Crear la figura y los ejes
+        fig, ax = plt.subplots(figsize=(self.width / 100, self.height / 100), dpi=100)
+        ax.set_xlim(x_min, x_max)
+        ax.set_ylim(y_min, y_max)
+        ax.set_aspect('equal')
+        ax.grid()
+
+        for shape_id, shape in self.scene.items():
+            shape.draw(ax)
+            if isinstance(shape, Triangulo):  # Verificar si la forma es un triángulo
+                ax.text(shape.params['vertex'][0][0], shape.params['vertex'][0][1], shape_id, fontsize=12)
+            else:  # Para las demás formas, imprimir el identificador en cada vértice
+                if shape.params.get('vertex'):  # Verificar si la forma tiene vértices definidos
+                    for vertex in shape.params['vertex']:
+                        ax.text(vertex[0], vertex[1], shape_id, fontsize=12)
+
+        plt.axis('equal')
+        plt.show()
--- src/controller/main.py
+++ src/controller/main.py
@@ -1,9 +1,25 @@
 import sys
 
 from antlr4 import FileStream, CommonTokenStream
+from ..Parser.GraphLangLexer import GraphLangLexer
+from ..Parser.GraphLangParser import GraphLangParser
+from ..Parser.CustomVisitor import CustomVisitor
 
 def main():
-    pass
+    file_path = 'input01.txt'
+    input_stream = FileStream(file_path)
+    lexer = GraphLangLexer(input_stream)
+    stream = CommonTokenStream(lexer)
+    parser = GraphLangParser(stream)
+    tree = parser.programa()
+
+    visitor = CustomVisitor()
+    visitor.visit(tree)
 
 if __name__ == "__main__":
-    pass
+    if len(sys.argv) != 2:
+        print("Usage: python main.py <input_file>")
+        sys.exit(1)
+
+    file_path = sys.argv[1]
+    main(file_path)
